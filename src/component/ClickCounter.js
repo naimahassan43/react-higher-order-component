c72ff093d1864100992d39c96ed4daf131d3c813
Naima Hassan <naima.hassan434@gmail.com>
2021-09-14 23:05:38 +0100

    Initialise project

---
+++ src/component/ClickCounter.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class ClickCounter extends React.Component {
+  render() {
+    return (
+      <div>
+        <button type="button">Clicked x times</button>
+      </div>
+    );
+  }
+}
+
+export default ClickCounter;
