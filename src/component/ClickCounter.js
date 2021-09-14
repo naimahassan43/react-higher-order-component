3ccf88fd0d1d32fc989b305c68ee37baecc0e3b8
Naima Hassan <naima.hassan434@gmail.com>
2021-09-14 23:16:22 +0100

    Click counter

--- src/component/ClickCounter.js
+++ src/component/ClickCounter.js
@@ -1,10 +1,21 @@
 import React from "react";
 
 class ClickCounter extends React.Component {
+  state = {
+    count: 0,
+  };
+
+  incrementCount = () => {
+    this.setState((prevState) => ({ count: prevState.count + 1 }));
+  };
+
   render() {
+    const { count } = this.state;
     return (
       <div>
-        <button type="button">Clicked x times</button>
+        <button type="button" onClick={this.incrementCount}>
+          Clicked {count} times
+        </button>
       </div>
     );
   }
