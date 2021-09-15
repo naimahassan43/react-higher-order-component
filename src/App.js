161db4b7206691d94e6bd38c807c6643148e74e6
Naima Hassan <naima.hassan434@gmail.com>
2021-09-15 23:02:55 +0100

    Render prop that defines render logic

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import ClickCounter from "./component/ClickCounter";
 import HoverCounter from "./component/HoverCounter";
+import User from "./component/User";
 
 function App() {
   return (
     <div className="app">
       <HoverCounter />
       <ClickCounter />
+      <User render={(isLoggedIn) => (isLoggedIn ? "Naima Hassan" : "Guest")} />
     </div>
   );
 }
