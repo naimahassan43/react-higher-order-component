cb660e74932531fc065da06f5799c6508d4e1ecf
Naima Hassan <naima.hassan434@gmail.com>
2021-09-15 23:30:13 +0100

    Convert into presentational component

--- src/component/ClickCounter.js
+++ src/component/ClickCounter.js
@@ -1,28 +1,9 @@
-// import withCounter from "./HOC/withCounter";
-
-// const ClickCounter = (props) => {
-//   const { count, incrementCount } = props;
-//   return (
-//     <div>
-//       <button type="button" onClick={incrementCount}>
-//         Clicked {count} times
-//       </button>
-//     </div>
-//   );
-// };
-
-// export default withCounter(ClickCounter);
-import React from "react";
-
-export default class ClickCounter extends React.Component {
-  render() {
-    const { count, incrementCount } = this.props;
-    return (
-      <div>
-        <button type="button" onClick={incrementCount}>
-          Clicked {count} times
-        </button>
-      </div>
-    );
-  }
+export default function ClickCounter({ count, incrementCount }) {
+  return (
+    <div>
+      <button type="button" onClick={incrementCount}>
+        Clicked {count} times
+      </button>
+    </div>
+  );
 }
--- src/component/HoverCounter.js
+++ src/component/HoverCounter.js
@@ -1,24 +1,7 @@
-// import withCounter from "./HOC/withCounter";
-
-// const HoverCounter = (props) => {
-//   const { count, incrementCount } = props;
-//   return (
-//     <div>
-//       <h1 onMouseOver={incrementCount}>Hovered {count} times</h1>
-//     </div>
-//   );
-// };
-
-// export default withCounter(HoverCounter);
-import React from "react";
-
-export default class HoverCounter extends React.Component {
-  render() {
-    const { count, incrementCount } = this.props;
-    return (
-      <div>
-        <h1 onMouseOver={incrementCount}>Hovered {count} times</h1>
-      </div>
-    );
-  }
+export default function HoverCounter({ count, incrementCount }) {
+  return (
+    <div>
+      <h1 onMouseOver={incrementCount}>Hovered {count} times</h1>
+    </div>
+  );
 }
