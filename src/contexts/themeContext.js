8228ca34b05f156174c8e52fae0ebcfea39e4839
Naima Hassan <naima.hassan434@gmail.com>
2021-09-20 23:44:36 +0100

    Using React context api

--- src/contexts/themeContext.js
+++ src/contexts/themeContext.js
@@ -1,7 +1,5 @@
-import createContext from "../lib/Context";
+import React from "react";
 
-const themeContext = createContext({
-  theme: "light",
-});
+const themeContext = React.createContext();
 
 export default themeContext;
